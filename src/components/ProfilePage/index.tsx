0dd553b101d01d4fac4f8a12b4420108c70c308d
Lucas <lmachado72@outlook.com>
2021-02-04 23:21:16 -0300

    profilePage main component

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Banner, 
+  Avatar, 
+  ProfileData, 
+  LocationIcon, 
+  CakeIcon, 
+  Followage,
+  EditButton
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar Perfil</EditButton>
+
+        <h1>jungkook</h1>
+        <h2>@th_ambtn</h2>
+
+        <p>
+          programador | ilustrador | comunista
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Curitiba, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 16 de dezembro de 1999
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>490</strong>
+          </span>
+          <span>
+            <strong>73 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+}
+
+export default ProfilePage;
